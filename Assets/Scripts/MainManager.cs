8e76b1cba60c04d0ca696e47cf0511bd27dc4104
Rafael de Moura Pernas <70914482+RafaelDeMoura880@users.noreply.github.com>
2022-08-21 10:13:15 -0300

    starting over

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -12,7 +12,6 @@ public class MainManager : MonoBehaviour
 
     public Text ScoreText;
     public GameObject GameOverText;
-    //public Text PlayerNameTxt;
     
     private bool m_Started = false;
     private int m_Points;
@@ -37,7 +36,6 @@ public class MainManager : MonoBehaviour
                 brick.onDestroyed.AddListener(AddPoint);
             }
         }
-        //PlayerNameTxt.text = "Best Score: " + MenuUIHandler.playerName; 
     }
 
     private void Update()
